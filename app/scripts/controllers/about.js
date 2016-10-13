85b31de486c3ab7d6753e5243eac8fe733c8b06f
Luis Arce <luis.arce22.a@gmail.com>
2016-10-12 20:56:26 -0400

    Start implementation to add sound in the game

--- app/scripts/controllers/about.js
+++ app/scripts/controllers/about.js
@@ -8,10 +8,20 @@
  * Controller of the myFirstGameApp
  */
 angular.module('myFirstGameApp')
-  .controller('AboutCtrl', function ($scope) {
-    $scope.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
-    ];
-  });
+    .controller('AboutCtrl', function($scope) {
+        $scope.awesomeThings = [
+            'HTML5 Boilerplate',
+            'AngularJS',
+            'Karma'
+        ];
+        $scope.soundID = "Thunder";
+        $scope.playSound = function() {
+            console.log("Play Sound");
+            createjs.Sound.play(soundID);
+        }
+
+        $scope.loadSound = function() {
+            console.log("Load Sound");
+            createjs.Sound.registerSound("images/0614.ogg", soundID);
+        }
+    });
