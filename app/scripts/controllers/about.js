92f5f9af6db63bff8fdb9e474526b881a9b161ee
Luis Arce <luis.arce22.a@gmail.com>
2016-10-13 20:15:11 -0400

    Udpated index.html to show the route contain and Updated about controller to play a sound after click in the play button

--- app/scripts/controllers/about.js
+++ app/scripts/controllers/about.js
@@ -14,14 +14,21 @@ angular.module('myFirstGameApp')
             'AngularJS',
             'Karma'
         ];
+
         $scope.soundID = "Thunder";
         $scope.playSound = function() {
             console.log("Play Sound");
-            createjs.Sound.play(soundID);
+            createjs.Sound.play($scope.soundID);
+            setTimeout($scope.stopSound, 1000);
+        }
+
+        $scope.stopSound = function() {
+            console.log("Stopped Sound");
+            createjs.Sound.stop($scope.soundID);
         }
 
         $scope.loadSound = function() {
             console.log("Load Sound");
-            createjs.Sound.registerSound("images/0614.ogg", soundID);
+            createjs.Sound.registerSound("images/0614.ogg", $scope.soundID);
         }
     });
